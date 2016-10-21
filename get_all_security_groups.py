fc8fffc80d04fdd6e1354c6b024831cae251d003
markusvanlaak <markusvanlaak@gmail.com>
2016-10-21 11:34:33 +0200

    Update get_all_security_groups.py

--- get_all_security_groups.py
+++ get_all_security_groups.py
@@ -4,6 +4,3 @@ ec2 = boto3.resource('ec2')
 
 security_groups = ec2.get_all_security_groups()
 print security_groups
-#security_group = ec2.SecurityGroup('sg-4ae39a22')
-
-#print security_group.group_name()
