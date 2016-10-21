8854aaa3149abb0787c7a6a9a19500d1e83bec74
markusvanlaak <markusvanlaak@gmail.com>
2016-10-21 12:14:43 +0200

    Update get_all_instances.py

--- get_all_instances.py
+++ get_all_instances.py
@@ -2,7 +2,11 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
+for i in ec2.instances.all(): print (i)
+
 instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 for instance in instances:
     print(instance.id, instance.instance_type)
+
+for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}]): print (i)
