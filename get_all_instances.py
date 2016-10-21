16a63ff403526350df759ef0f191f18ebf3470e4
markusvanlaak <markusvanlaak@gmail.com>
2016-10-21 12:47:08 +0200

    Update get_all_instances.py

--- get_all_instances.py
+++ get_all_instances.py
@@ -10,3 +10,15 @@ for instance in instances:
     print(instance.id, instance.instance_type)
 
 for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}]): print (i)
+
+    
+#add tags
+
+mytags = [{
+    "Key" : "Name",
+            "Value" : "GIG 3"
+    }]
+
+for i in ec2.security_groups.all(): id.append(i.id)
+    for id_sg in id:
+        ec2.create_tags(Resources = [id_sg],Tags= mytags)
